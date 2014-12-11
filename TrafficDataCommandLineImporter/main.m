a48f23a6b9257a2a4af978e7b8ee890fb6de1cbe
yangyao <yangyao@myhexin.com>
2014-12-11 11:38:46 +0800

    Initial Commit

---
+++ TrafficDataCommandLineImporter/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TrafficDataCommandLineImporter
+//
+//  Created by Wild Yaoyao on 14/12/11.
+//  Copyright (c) 2014年 Yang Yao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
